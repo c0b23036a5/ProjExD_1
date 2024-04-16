0a8430cc39f0b09efd31152b59e837216fd66585
Nobunagay <c0b23036a5@edu.teu.ac.jp>
2024-04-16 17:26:36 +0900

    演習2問目おしまい

--- pygame_image.py
+++ pygame_image.py
@@ -23,31 +23,53 @@ def main():
     R_=1
     U_=1
     D_=1
+    l_=0
+    r_=0
+    u_=0
+    d_=0
+    l_p=0
+    r_p=0
+    u_p=0
+    d_p=0
     while True:
         
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst=pg.key.get_pressed()
-        if key_lst[pg.K_DOWN]:
+        
+
+        if key_lst[pg.K_DOWN] and (d_==0 or d_p<d_):
             Kktn_rct.move_ip((0,+1))
+            d_p=d_
             D_=0
+            d_+=1
         else:
             D_=1
-        if key_lst[pg.K_LEFT]:
+            d_=d_p
+        if key_lst[pg.K_LEFT] and (l_==0 or l_p<l_):
             Kktn_rct.move_ip((-1,0))
             L_=0
+            l_p=l_
+            l_+=1
         else:
             L_=1
-        if key_lst[pg.K_RIGHT]:
+            l_=l_p
+        if key_lst[pg.K_RIGHT] and (r_==0 or r_p<r_):
             Kktn_rct.move_ip((+2,0))
             R_=0
+            r_p=r_
+            r_+=1
         else:
             R_=1
-        if key_lst[pg.K_UP]:
+            r_=r_p
+        if key_lst[pg.K_UP] and (u_==0 or u_p<u_):
             Kktn_rct.move_ip((0,-1))
             U_=0
+            u_p=u_
+            u_+=1
         else:
             U_=1
+            u_=u_p
         if L_+R_+U_+D_==4:
             Kktn_rct.move_ip((-1,0))
             
