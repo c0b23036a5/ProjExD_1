61565e97720e01461899f421e7bdb18ae870d35a
Nobunagay <c0b23036a5@edu.teu.ac.jp>
2024-04-16 18:31:13 +0900

    演習2問目おしまい

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -19,18 +19,12 @@ def main():
     scr_x=0
     Kktn_rct= Kktn.get_rect()
     Kktn_rct.center=300,200
-    L_=1
-    R_=1
-    U_=1
-    D_=1
-    l_=0
-    r_=0
-    u_=0
-    d_=0
-    l_p=0
-    r_p=0
-    u_p=0
-    d_p=0
+    
+    X=0
+    Y=0
+    F=0
+    L_=0
+    R_=0
     while True:
         
         for event in pg.event.get():
@@ -38,40 +32,35 @@ def main():
         key_lst=pg.key.get_pressed()
         
 
-        if key_lst[pg.K_DOWN] and (d_==0 or d_p<d_):
-            Kktn_rct.move_ip((0,+1))
-            d_p=d_
-            D_=0
-            d_+=1
+        
+        L_=0
+        R_=0
+        X=0
+        Y=0
+        if key_lst[pg.K_LEFT] :
+            X-=1
         else:
-            D_=1
-            d_=d_p
-        if key_lst[pg.K_LEFT] and (l_==0 or l_p<l_):
-            Kktn_rct.move_ip((-1,0))
-            L_=0
-            l_p=l_
-            l_+=1
+            L_+=1
+        if key_lst[pg.K_RIGHT] :
+            X+=2
         else:
-            L_=1
-            l_=l_p
-        if key_lst[pg.K_RIGHT] and (r_==0 or r_p<r_):
-            Kktn_rct.move_ip((+2,0))
-            R_=0
-            r_p=r_
-            r_+=1
+            L_+=1
+    
+        if key_lst[pg.K_UP]:
+            Y-=1
         else:
-            R_=1
-            r_=r_p
-        if key_lst[pg.K_UP] and (u_==0 or u_p<u_):
-            Kktn_rct.move_ip((0,-1))
-            U_=0
-            u_p=u_
-            u_+=1
+            R_+=1
+        if key_lst[pg.K_DOWN]:
+            
+            Y+=1
         else:
-            U_=1
-            u_=u_p
-        if L_+R_+U_+D_==4:
-            Kktn_rct.move_ip((-1,0))
+            R_+=1
+        F=0    
+        if L_+R_==4:
+            F=-1
+        Kktn_rct.move_ip((F+X,Y))
+        
+        
             
         if scr_x<=-3200:
             scr_x=0
