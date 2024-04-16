28a1bfe3b59a6d69eadf541aced2fbbcab5c5282
Nobunagay <c0b23036a5@edu.teu.ac.jp>
2024-04-16 16:05:56 +0900

    7問目おしまい

--- pygame_image.py
+++ pygame_image.py
@@ -11,29 +11,33 @@ def main():
     clock  = pg.time.Clock()
     clock.tick(200)
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    Rbg_img = pg.transform.flip(bg_img,True,False)
     Kktn = pg.image.load("fig/3.png")
     Kktn = pg.transform.flip(Kktn,True,False)
     tmr = 0
-    slide_x=0
+    x=0
     scr_x=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         
-        if scr_x<=-800:
+        if scr_x<=-3200:
             scr_x=0
-            slide_x=0
-        scr_x=-slide_x*20
+            x=0
+        scr_x=-x*10
         
 
         screen.blit(bg_img, [scr_x, 0])
+        screen.blit(Rbg_img,[scr_x+1600,0])
+        screen.blit(bg_img, [scr_x+3200, 0])
+        screen.blit(Rbg_img,[scr_x+4800,0])
         screen.blit(Kktn,[300,200])
         
 
         pg.display.update()
         tmr += 1 
-        slide_x +=1       
+        x +=1       
         clock.tick(10)
 
 
